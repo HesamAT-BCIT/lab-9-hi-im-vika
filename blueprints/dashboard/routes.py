840fbb7c80318630fd46852060c5c21f4b253299
vika <37137412+hi-im-vika@users.noreply.github.com>
2026-03-07 13:23:52 -0800

    Fix dashboard blueprint imports

--- blueprints/dashboard/routes.py
+++ blueprints/dashboard/routes.py
@@ -1,6 +1,7 @@
-import utils.profile, utils.validation, utils.auth
+from utils.auth import get_current_user
+from utils.profile import get_profile_data
+from flask import session, redirect, url_for, render_template
 from . import dashboard_bp
-from flask import request, url_for, redirect, render_template
 
 @dashboard_bp.route("/")
 def home():
@@ -9,4 +10,4 @@ def home():
     if current_user:
         profile_data = get_profile_data(current_user)
         return render_template("dashboard.html", first_name=profile_data.get('first_name', ''), jwt_token=session.get('jwt_token'))
-    return redirect(url_for("login"))
+    return redirect(url_for("auth.login"))
